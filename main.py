7479f141aae748328e04876b175cede7ebc0640f
masewo <masewo@gmx.net>
2021-06-08 00:33:27 +0200

    adaptions for other device sizes

--- main.py
+++ main.py
@@ -25,14 +25,16 @@ def merge_with_background(i, folder, path, name):
     else:
         landscape = False
 
-    image_size = (2160, 3840)
-    # image_size = (2048, 2732)
+    image_size = (2160, 3840)  # android
+    # image_size = (2048, 2732)  # ipad
+    # image_size = (1284, 2778)  # iphone 6,5
+    # image_size = (1242, 2208)  # iphone 5,5
 
     if landscape:
         image_size = image_size[::-1]
 
-    percent_of_height_visible = 0.78
-    percent_of_height_padding = 0.04
+    percent_of_height_visible = 0.80  # change me
+    percent_of_height_padding = 0.01  # change me
 
     percent_of_height_for_screenshot = percent_of_height_visible - 2 * percent_of_height_padding
 
@@ -45,7 +47,7 @@ def merge_with_background(i, folder, path, name):
     background_path = 'input\\backgrounds\\' + str(i) + '_' + ('land' if landscape else 'port') + '.svg'
     background = convert_to_png(background_path, image_size)
 
-    new_image = Image.new('RGBA', (image_size[0], image_size[1]), (255, 0, 0, 0))
+    new_image = Image.new('RGBA', (image_size[0], image_size[1]), (1, 161, 145, 255))
 
     offset = (
         int(image_size[0] / 2 - scaled_screenshot_size[0] / 2),
@@ -61,7 +63,7 @@ def convert_to_png(path, size):
     out = BytesIO()
     cairosvg.svg2png(url=path, write_to=out,
                      output_width=size[0],
-                     output_height=size[1],
+                     # output_height=size[1],  # change me
                      # parent_width=size[0],
                      # parent_height=size[1],
                      background_color="rgba(1, 161, 145, 255)"
